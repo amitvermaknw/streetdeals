477093f74542afa08a1738a0f7085b92eb1ec5f4
Amit Verma <amitfromus@gmail.com>
2024-04-27 17:17:50 -0700

    fixed issue for product detail page

--- src/features/pdetails/components/PDetails.tsx
+++ src/features/pdetails/components/PDetails.tsx
@@ -2,13 +2,14 @@ import { useEffect } from "react";
 import useProductDetails from "../hooks/useProductDeatails";
 import YouMayLike from "./YouMayLike";
 import GetDealsDetailModel from "../../../model/GetDealsDetailModel";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Skeleton from "../../../components/ui/Skeleton";
 import { toast } from "react-toastify";
 
 const PDetails = () => {
     const [pstate, getDealDetails] = useProductDetails(GetDealsDetailModel);
     const { pid } = useParams();
+    const navigate = useNavigate();
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const getDeals = (pid: any) => {
@@ -25,82 +26,89 @@ const PDetails = () => {
     }
 
     return (
-        pstate.pname ? <div className="font-[sans-serif]">
-            <div className="p-6 lg:max-w-7xl max-w-2xl max-lg:mx-auto">
-                <div className="grid items-start grid-cols-1 lg:grid-cols-5 gap-12">
-                    <div className="lg:col-span-3 bg-gray-100 w-full lg:sticky top-0 text-center p-8">
-                        <img src={pstate.pimageurl} alt="Product" className="w-4/5 rounded object-cover" />
-                        {/* <hr className="border-white border-2 my-6" />
+        pstate.pname ? <>
+
+
+            <div className="font-[sans-serif]">
+                <div className="p-6 lg:max-w-7xl max-w-2xl max-lg:mx-auto">
+                    <button type="button"
+                        onClick={() => navigate('/')}
+                        className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                        Back</button>
+                    <div className="grid items-start grid-cols-1 lg:grid-cols-5 gap-12">
+                        <div className="lg:col-span-3 bg-gray-100 w-full lg:sticky top-0 text-center p-8">
+                            <img src={pstate.pimageurl} alt="Product" className="w-4/5 rounded object-cover" />
+                            {/* <hr className="border-white border-2 my-6" />
                         <div className="flex flex-wrap gap-x-12 gap-y-6 justify-center mx-auto">
                             <img src="https://readymadeui.com/images/coffee6.webp" alt="Product1" className="w-24 cursor-pointer" />
                             <img src="https://readymadeui.com/images/coffee3.webp" alt="Product2" className="w-24 cursor-pointer" />
                             <img src="https://readymadeui.com/images/coffee4.webp" alt="Product3" className="w-24 cursor-pointer" />
                             <img src="https://readymadeui.com/images/coffee5.webp" alt="Product4" className="w-24 cursor-pointer" />
                         </div> */}
-                    </div>
-                    <div className="lg:col-span-2">
-                        <h2 className="text-2xl font-extrabold text-gray-800">{pstate.pname}</h2>
-                        <div className="flex flex-wrap gap-4 mt-4">
-                            <p className="text-gray-400 text-xl"><del>${pstate.preprice}</del></p>
-                            <p className="text-green-600 text-xl font-bold">${pstate.price}</p>
-                            <div className="mr-2 rounded-2xl bg-red-700 py-1.5 px-4 text-xs text-white md:ml-2 xl:ml-2">Limited time deal</div>
+                        </div>
+                        <div className="lg:col-span-2">
+                            <h2 className="text-2xl font-extrabold text-gray-800">{pstate.pname}</h2>
+                            <div className="flex flex-wrap gap-4 mt-4">
+                                <p className="text-gray-400 text-xl"><del>${pstate.preprice}</del></p>
+                                <p className="text-green-600 text-xl font-bold">${pstate.price}</p>
+                                <div className="mr-2 rounded-2xl bg-red-700 py-1.5 px-4 text-xs text-white md:ml-2 xl:ml-2">Limited time deal</div>
 
-                            <span className="text-sm ml-1 text-gray-400">Deal can be end anytime.</span>
-                        </div>
-                        <div className="flex space-x-2 mt-4">
-                            <svg className="w-5 fill-gray-800" viewBox="0 0 14 13" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                            </svg>
-                            <svg className="w-5 fill-gray-800" viewBox="0 0 14 13" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                            </svg>
-                            <svg className="w-5 fill-gray-800" viewBox="0 0 14 13" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                            </svg>
-                            <svg className="w-5 fill-gray-800" viewBox="0 0 14 13" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                            </svg>
-                            <svg className="w-5 fill-[#CED5D8]" viewBox="0 0 14 13" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                            </svg>
-                        </div>
-                        <div className="mt-8">
-                            {pstate.coupon ? <p className="text-base leading-4 mt-7 text-gray-600 dark:text-gray-300">
-                                Coupon: <a href="#"
-                                    onClick={() => copyCoupon(pstate.coupon as string)}
-                                    className="bg-blue-100 hover:bg-blue-200 text-blue-800 text-xs font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400 inline-flex items-center justify-center">
-                                    {pstate.coupon}
-                                </a>
-                                <span
-                                    onClick={() => copyCoupon(pstate.coupon as string)}
-                                    className="text-sm ml-1 text-gray-400" >Click here to copy</span>
-                            </p> : ''}
-                        </div>
-                        <div className="mt-8">
-                            <h3 className="text-lg font-bold text-gray-800">Details</h3>
-                            <div className="pdetails" dangerouslySetInnerHTML={{ __html: pstate.productdetails }} />
+                                <span className="text-sm ml-1 text-gray-400">Deal can be end anytime.</span>
+                            </div>
+                            <div className="flex space-x-2 mt-4">
+                                <svg className="w-5 fill-gray-800" viewBox="0 0 14 13" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                                </svg>
+                                <svg className="w-5 fill-gray-800" viewBox="0 0 14 13" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                                </svg>
+                                <svg className="w-5 fill-gray-800" viewBox="0 0 14 13" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                                </svg>
+                                <svg className="w-5 fill-gray-800" viewBox="0 0 14 13" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                                </svg>
+                                <svg className="w-5 fill-[#CED5D8]" viewBox="0 0 14 13" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                                </svg>
+                            </div>
+                            <div className="mt-8">
+                                {pstate.coupon ? <p className="text-base leading-4 mt-7 text-gray-600 dark:text-gray-300">
+                                    Coupon: <a href="#"
+                                        onClick={() => copyCoupon(pstate.coupon as string)}
+                                        className="bg-blue-100 hover:bg-blue-200 text-blue-800 text-xs font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400 inline-flex items-center justify-center">
+                                        {pstate.coupon}
+                                    </a>
+                                    <span
+                                        onClick={() => copyCoupon(pstate.coupon as string)}
+                                        className="text-sm ml-1 text-gray-400" >Click here to copy</span>
+                                </p> : ''}
+                            </div>
+                            <div className="mt-8">
+                                <h3 className="text-lg font-bold text-gray-800">Details</h3>
+                                <div className="pdetails" dangerouslySetInnerHTML={{ __html: pstate.productdetails }} />
 
-                            {/* <ul className="space-y-3 list-disc mt-4 pl-4 text-sm text-gray-800">
+                                {/* <ul className="space-y-3 list-disc mt-4 pl-4 text-sm text-gray-800">
                                 <li>A cup of coffee is a beverage essential because of its timeless appeal</li>
                                 <li>Easy to prepare. It can be brewed using various methods, from drip machines to manual pour-overs.</li>
                                 <li>Available in various sizes, from a standard espresso shot to a large Americano, catering to different preferences.</li>
                                 <li>You can customize your coffee by adding cream, sugar, or flavorings to suit your taste preferences.</li>
                             </ul> */}
-                        </div>
+                            </div>
 
-                        <div className="mt-8 max-w-md">
-                            {/* <h3 className="text-lg font-bold text-gray-800">Reviews(10)</h3> */}
-                            {/* <div className="space-y-3 mt-4">
+                            <div className="mt-8 max-w-md">
+                                {/* <h3 className="text-lg font-bold text-gray-800">Reviews(10)</h3> */}
+                                {/* <div className="space-y-3 mt-4">
                                 <div className="flex items-center">
                                     <p className="text-sm text-gray-800 font-bold">5.0</p>
                                     <svg className="w-5 fill-gray-800 ml-1" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -157,7 +165,7 @@ const PDetails = () => {
                                     <p className="text-sm text-gray-800 font-bold ml-3">6%</p>
                                 </div>
                             </div> */}
-                            {/* <div className="flex items-start mt-8">
+                                {/* <div className="flex items-start mt-8">
                                 <img src="https://readymadeui.com/team-2.webp" className="w-12 h-12 rounded-full border-2 border-white" />
                                 <div className="ml-3">
                                     <h4 className="text-sm font-bold">John Doe</h4>
@@ -192,17 +200,17 @@ const PDetails = () => {
                                     <p className="text-xs mt-4">The service was amazing. I never had to wait that long for my food. The staff was friendly and attentive, and the delivery was impressively prompt.</p>
                                 </div>
                             </div> */}
-                            <button type="button"
-                                className="w-full mt-8 px-4 py-2 bg-transparent border-2 border-gray-800 text-gray-800 font-bold rounded"
-                                onClick={() => window.open(pstate.producturl, '_blank')}
-                            >Get the deal</button>
+                                <button type="button"
+                                    className="w-full mt-8 px-4 py-2 bg-transparent border-2 border-gray-800 text-gray-800 font-bold rounded"
+                                    onClick={() => window.open(pstate.producturl, '_blank')}
+                                >Get the deal</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <hr className="mt-16 mb-4"></hr>
-            <YouMayLike />
-        </div > : <Skeleton />
+                <hr className="mt-16 mb-4"></hr>
+                <YouMayLike />
+            </div > </> : <Skeleton />
     )
 }
 
