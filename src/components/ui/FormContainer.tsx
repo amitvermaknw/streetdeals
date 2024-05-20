505c97db66550ca491b305e2dfe9bd1986e4d3b4
Amit Verma <amitfromus@gmail.com>
2024-05-20 11:25:57 -0700

    fixed admin dropdown issue

--- src/components/ui/FormContainer.tsx
+++ src/components/ui/FormContainer.tsx
@@ -42,7 +42,7 @@ const buildForm = (formElement: Props) => {
 
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const FormContainer = (props: Props) => {
+const FormContainer = ({ ...props }: Props) => {
     return (<>
         {typeof props.state === 'object' ? buildForm(props).map(item => item) : ''}
     </>)
